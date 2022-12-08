fdf155befb6a06875b29ea1710cdfdd881275fc8
Roy Smith <roy@panix.com>
2022-12-07 19:16:18 -0500

    Refactor fixtures into conftest.py

---
+++ dyk_tools/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import pytest_socket
+
+import pywikibot
+
+
+@pytest.fixture
+def site(monkeypatch, mocker):
+    """Returns a mock pywikibot.Site instance.  Also closes off some possible
+    network leakage paths in case something bypasses the mock.
+
+    """
+    pytest_socket.disable_socket()
+    monkeypatch.setattr(pywikibot.config, "max_retries", 0)
+    mock_Site = mocker.patch("pywikibot.Site", autoconfig=pywikibot.site.APISite)
+    return mock_Site()
+
+
+@pytest.fixture
+def page(mocker, site):
+    """Returns a mock pywikibot.Page."""
+    mock_Page = mocker.patch("pywikibot.Page", autospec=True)
+    return mock_Page(site)
+    
--- dyk_tools/test_nomination.py
+++ dyk_tools/test_nomination.py
@@ -1,27 +1,7 @@
 import pytest
-import pytest_socket
 import pywikibot
 
 from dyk_tools import Nomination, Hook
-
-
-@pytest.fixture
-def site(monkeypatch, mocker):
-    """Returns a mock pywikibot.Site instance.  Also closes off some possible
-    network leakage paths in case something bypasses the mock.
-
-    """
-    pytest_socket.disable_socket()
-    monkeypatch.setattr(pywikibot.config, "max_retries", 0)
-    mock_Site = mocker.patch("pywikibot.Site", autoconfig=pywikibot.site.APISite)
-    return mock_Site()
-
-
-@pytest.fixture
-def page(mocker, site):
-    """Returns a mock pywikibot.Page."""
-    mock_Page = mocker.patch("pywikibot.Page", autospec=True)
-    return mock_Page(site)
 
 
 def icon(mocker, name):
