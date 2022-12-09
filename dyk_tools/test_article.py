544ee851d11c35303040f28e806a430d36dba29a
Roy Smith <roy@panix.com>
2022-12-08 20:56:29 -0500

    Fix test

--- dyk_tools/test_article.py
+++ dyk_tools/test_article.py
@@ -7,15 +7,15 @@ def test_article_can_be_constructed(page):
     article = Article(page)
 
 
-def test_has_birth_data_returns_false_with_no_categories(page):
+def test_has_birth_category_returns_false_with_no_categories(page):
     article = Article(page)
     page.categories.return_value = []
-    assert article.is_biography() == False
+    assert article.has_birth_category() == False
 
 
-def test_has_birth_data_returns_true_with_year_of_birth_category(mocker, page):
+def test_has_birth_category_returns_true_with_birth_category(mocker, page):
     article = Article(page)
     cat = mocker.Mock(spec=pywikibot.Category)
     cat.title.return_value = "1944 births"
     page.categories.return_value = [cat]
-    assert article.is_biography() == True
+    assert article.has_birth_category() == True
