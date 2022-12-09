f77d2a1d771b2a90e31623f5f8edea94be8337b6
Roy Smith <roy@panix.com>
2022-12-08 20:39:42 -0500

    Refactor is_biography()

--- dyk_tools/article.py
+++ dyk_tools/article.py
@@ -7,6 +7,9 @@ class Article:
     page: Page
 
     def is_biography(self) -> bool:
+        return self.has_birth_category()
+
+    def has_birth_category(self) -> bool:
         for cat in self.page.categories():
             if cat.title().endswith(' births'):
                 return True
--- dyk_tools/test_article.py
+++ dyk_tools/test_article.py
@@ -7,13 +7,13 @@ def test_article_can_be_constructed(page):
     article = Article(page)
 
 
-def test_is_biography_returns_false_with_no_categories(page):
+def test_has_birth_data_returns_false_with_no_categories(page):
     article = Article(page)
     page.categories.return_value = []
     assert article.is_biography() == False
 
 
-def test_is_biography_returns_true_with_year_of_birth_category(mocker, page):
+def test_has_birth_data_returns_true_with_year_of_birth_category(mocker, page):
     article = Article(page)
     cat = mocker.Mock(spec=pywikibot.Category)
     cat.title.return_value = "1944 births"
