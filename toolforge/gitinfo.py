233556a5180c3bba148ae0c54d3d4fa1b3217425
tools.dyk-tools <tools.dyk-tools@tools-sgebastion-11.tools.eqiad1.wikimedia.cloud>
2022-12-20 18:49:13 +0000

    Add gitinfo.py.

---
+++ toolforge/gitinfo.py
@@ -0,0 +1,48 @@
+# Utilities for interfacing with git source control.
+
+import sh
+
+def get_info():
+    """Returns a string describing the current state of the checked-out
+    source tree.  The exact format of the string is unspecified,
+    other than being suitable for display to an end user as a
+    tracking and error-reporting aid.
+
+    """
+    # pylint: disable=too-many-function-args
+
+    data, dirty = get_status()
+    head = data['branch.head']
+    oid = data['branch.oid']
+    dirty_flag = '+' if dirty else ''
+    tag_list = get_tags(oid)
+    tags = f" [{', '.join(tag_list)}]" if tag_list else ""
+    return f"{head} ({oid}{dirty_flag}{tags})"
+
+
+def get_status():
+    """Returns a (data, dirty) tuple, where data is a dict containing the status fields
+    and dirty is True if there are any uncommitted changes pending.
+
+    """
+    output = sh.git('--no-pager', 'status', '--porcelain=v2', '-b')
+    dirty = False
+    data = {}
+    for line in output:
+        if line.startswith('#'):
+            _, key, value = line.rstrip().split(' ', 2)
+            data[key] = value
+        else:
+            dirty = True
+    return (data, dirty)
+
+
+def get_tags(oid):
+    """Returns a list of tags which point to the oid (which should be a string).
+
+    """
+    output = sh.git('--no-pager', 'tag', '--points-at', oid)
+    return [tag.rstrip() for tag in output]
+
+if __name__ == '__main__':
+    print(get_info())
