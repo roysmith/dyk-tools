1f1a085f09925b9f044055e398df94d87a7705e9
Roy Smith <roy@panix.com>
2023-01-07 14:34:26 -0500

    Run dykbot as an installed script.

--- dyk_tools/bot/dykbot.bash
+++ dyk_tools/bot/dykbot.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 source $HOME/www/python/venv/bin/activate
-cd $HOME/www/python/src
-./dykbot.py \
+dykbot \
     --no-dry-run \
     --log-level=debug \
     --log-file=$HOME/dykbot.log
