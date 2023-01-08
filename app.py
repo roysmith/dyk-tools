1541d5eca16650cdbec6f7445bf52f9438351022
Roy Smith <roy@panix.com>
2023-01-07 22:55:33 -0500

    Move web application into its own directory.

--- app.py
+++ app.py
@@ -1,35 +1,3 @@
-from flask import Flask, render_template, request, redirect, url_for
-from pywikibot import Site, Page, Category
-from template_form import TemplateForm
-from dyk_tools import Nomination, Article
+import dyk_web.app
 
-app = Flask(__name__)
-
-SITE = Site("en", "wikipedia", "dyk-tools")
-
-
-@app.route("/", methods=["GET", "POST"])
-def home_page():
-    form = TemplateForm(request.form)
-    if request.method == "POST" and form.validate():
-        return redirect(url_for("display", template_name=form.name.data))
-    form.name.choices = get_pending_nominations()
-    return render_template("home_page.html", form=form)
-
-
-def get_pending_nominations():
-    cat = Category(SITE, "Pending DYK nominations")
-    titles = []
-    for nom in cat.articles():
-        title = nom.title()
-        titles.append((title, title.removeprefix("Template:Did you know nominations/")))
-    return titles
-
-
-@app.route("/display")
-def display():
-    """template_name query arg is the DYK nomination template, including the Template: prefix."""
-    page = Page(SITE, request.args["template_name"])
-    nomination = Nomination(page)
-    articles = [Article(a) for a in nomination.articles()]
-    return render_template("display.html", nomination=nomination, articles=articles)
+app = dyk_web.app.app
