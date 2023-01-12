e78514e2f35ec9d5c250d47f2245db49e4c2aec1
Roy Smith <roy@panix.com>
2023-01-12 09:58:20 -0500

    Add startup logging

--- dyk_web/app.py
+++ dyk_web/app.py
@@ -1,11 +1,12 @@
 import logging.config
+import os
 from pathlib import Path
 
 from flask import Flask, redirect, url_for, g
 from pywikibot import Site
 
 from . import core, api
-from .cache import cache
+from .cache import cache, cache_config
 
 # fmt: off
 logging.config.dictConfig(
@@ -32,6 +33,9 @@ app = Flask(__name__)
 app.register_blueprint(core.bp)
 app.register_blueprint(api.bp)
 cache.init_app(app)
+
+app.logger.info(f"Running on {os.uname().nodename}")
+app.logger.info(f"Using {cache_config['CACHE_TYPE']}")
 
 
 @app.before_request
