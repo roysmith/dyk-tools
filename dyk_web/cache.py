6f9e6e2337bae4aa29adac0d7c879486b2d11775
Roy Smith <roy@panix.com>
2023-01-12 10:14:36 -0500

    Configure RedisCache

--- dyk_web/cache.py
+++ dyk_web/cache.py
@@ -1,5 +1,19 @@
+import os
+
 from flask_caching import Cache
 
-config = {"CACHE_TYPE": "SimpleCache"}
+from dyk_tools.version import version_string
 
-cache = Cache(config=config)
+
+# Note: CACHE_KEY_PREFIX deliberately includes version_string
+# so all cache entries are invalidated on any version change.
+if "REDIS_CACHE_URL" in os.environ:
+    cache_config = {
+        "CACHE_TYPE": "RedisCache",
+        "CACHE_KEY_PREFIX": f"dyk-tools-{version_string}",
+        "CACHE_REDIS_URL": os.environ["REDIS_CACHE_URL"],
+    }
+else:
+    cache_config = {"CACHE_TYPE": "SimpleCache"}
+
+cache = Cache(config=cache_config)
