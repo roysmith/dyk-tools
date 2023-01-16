601b8a9d7f8856e0ba38a774d3417f59d5e847e2
Roy Smith <roy@panix.com>
2023-01-15 22:30:18 -0500

    Refactor MockPage and MockCategory

--- tests/test_dyk_tools/test_article.py
+++ tests/test_dyk_tools/test_article.py
@@ -5,6 +5,16 @@ import dyk_tools.article
 
 
 @pytest.fixture(autouse=True)
+def MockPage(mocker):
+    return mocker.patch("dyk_tools.article.Page", autospec=True)
+
+
+@pytest.fixture(autouse=True)
+def MockCategory(mocker):
+    return mocker.patch("dyk_tools.article.Category", autospec=True)
+
+
+@pytest.fixture(autouse=True)
 def clear_cache():
     dyk_tools.article.get_biography_infobox_templates.cache_clear()
 
@@ -42,51 +52,45 @@ class TestHasBirthCategory:
 
 
 class TestHasPersonInfobox:
-    def test_has_person_infobox_returns_false_with_no_infobox(self, mocker, page1):
-        mocker.patch("dyk_tools.article.Category", autospec=True)
-        Mock_Page = mocker.patch("dyk_tools.article.Page", autospec=True)
+    def test_has_person_infobox_returns_false_with_no_infobox(self, page1):
         article = Article(page1)
         page1.templates.return_value = []
         assert article.has_person_infobox() == False
 
     def test_has_person_infobox_returns_true_with_correct_infobox(
-        self, mocker, page1, page2
+        self, mocker, MockCategory, MockPage, page1, page2
     ):
         page1.templates.return_value = [page2]
         page2.title.return_value = "Template:Infobox A"
-        Mock_Category = mocker.patch("dyk_tools.article.Category", autospec=True)
-        Mock_Page = mocker.patch("dyk_tools.article.Page", autospec=True)
-        infobox_cat = Mock_Category(None, None)
+        infobox_cat = MockCategory(None, None)
         infobox_cat.articles.return_value = [page2]
         mocker.resetall()
         article = Article(page1)
 
         assert article.has_person_infobox() == True
-        Mock_Category.assert_called_once_with(
+        MockCategory.assert_called_once_with(
             page1.site, "People and person infobox templates"
         )
         infobox_cat.articles.assert_called_once_with(recurse=3, namespaces=[mocker.ANY])
-        Mock_Page.assert_any_call(page1.site, "Template:Infobox character")
-        Mock_Page.assert_any_call(page1.site, "Template:Infobox comics character")
+        MockPage.assert_any_call(page1.site, "Template:Infobox character")
+        MockPage.assert_any_call(page1.site, "Template:Infobox comics character")
 
     def test_has_person_infobox_returns_false_with_unknown_infobox(
-        self, mocker, page1, page2, page3, page4
+        self, mocker, MockCategory, MockPage, page1, page2, page3, page4
     ):
         page1.templates.return_value = [page2]
-        Mock_Category = mocker.patch("dyk_tools.article.Category", autospec=True)
-        Mock_Page = mocker.patch("dyk_tools.article.Page", autospec=True)
-        infobox_cat = Mock_Category(None, None)
+        infobox_cat = MockCategory(None, None)
         infobox_cat.articles.return_value = [page3, page4]
         mocker.resetall()
         article = Article(page1)
 
         assert article.has_person_infobox() == False
-        Mock_Category.assert_called_once_with(
+        MockCategory.assert_called_once_with(
             page1.site, "People and person infobox templates"
         )
         infobox_cat.articles.assert_called_once_with(recurse=3, namespaces=[mocker.ANY])
-        Mock_Page.assert_any_call(page1.site, "Template:Infobox character")
-        Mock_Page.assert_any_call(page1.site, "Template:Infobox comics character")
+        MockPage.assert_any_call(page1.site, "Template:Infobox character")
+        MockPage.assert_any_call(page1.site, "Template:Infobox comics character")
 
     def test_has_american_short_description_returns_false_with_no_short_description(
         self, page1
@@ -147,16 +151,20 @@ class TestIsAmerican:
         article = Article(page1)
         assert article.is_american() == False
 
-    def test_is_american_returns_true_with_united_states_category(self, mocker, page1):
-        cat = mocker.Mock(spec=pywikibot.Category)
+    def test_is_american_returns_true_with_united_states_category(
+        self, mocker, MockCategory, page1
+    ):
+        cat = MockCategory(None, None)
         cat.title.return_value = "Things in the united states"
         page1.categories.return_value = [cat]
         page1.extract.return_value = ""
         article = Article(page1)
         assert article.is_american() == True
 
-    def test_is_american_returns_true_with_other_category(self, mocker, page1):
-        cat = mocker.Mock(spec=pywikibot.Category)
+    def test_is_american_returns_true_with_other_category(
+        self, mocker, MockCategory, page1
+    ):
+        cat = MockCategory(None, None)
         cat.title.return_value = "Things in lower slobbovia"
         page1.categories.return_value = [cat]
         page1.extract.return_value = ""
