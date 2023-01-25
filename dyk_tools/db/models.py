5de0eab66b84929e0fdabe227e14a7f487152e4b
Roy Smith <roy@panix.com>
2023-01-25 15:43:01 -0500

    Mysql requires max field length for varchars.

--- dyk_tools/db/models.py
+++ dyk_tools/db/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from pprint import pprint
 from typing import Optional
 
+from sqlalchemy import String
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -19,5 +20,5 @@ class BotLog(BaseModel):
     __tablename__ = "bot_log"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(index=True)
+    title: Mapped[str] = mapped_column(String(255), index=True)
     timestamp_utc: Mapped[datetime]
