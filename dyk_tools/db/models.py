c165013e827980238695c41391434f7ad45a4dd5
Roy Smith <roy@panix.com>
2023-01-27 16:12:37 -0500

    Restore full BotLog.title, use index prefix.

--- dyk_tools/db/models.py
+++ dyk_tools/db/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from pprint import pprint
 from typing import Optional
 
-from sqlalchemy import String
+from sqlalchemy import String, Index
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -20,5 +20,8 @@ class BotLog(BaseModel):
     __tablename__ = "bot_log"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(String(190), index=True)
+    title: Mapped[str] = mapped_column(String(255))
     timestamp_utc: Mapped[datetime]
+
+    # 190 prefix length avoids 767 byte index limit.
+    __table_args__ = (Index("ix_title", "title", mysql_length=190),)
