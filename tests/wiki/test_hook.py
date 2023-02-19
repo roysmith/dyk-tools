e2fb3a5ae797679b4da746fd160be3fe56386d9a
Roy Smith <roy@panix.com>
2023-02-18 21:17:18 -0500

    Refactor TestRender

--- tests/wiki/test_hook.py
+++ tests/wiki/test_hook.py
@@ -21,39 +21,37 @@ def test_construct():
     assert hook.text == "text"
 
 
-@pytest.fixture(
-    params=[
-        ("", ""),
-        ("foo", "foo"),
-        ("[[foo]]", '<a href="my url">foo</a>'),
-        ("'''foo'''", "<b>foo</b>"),
-        ("'''[[foo]]'''", '<b><a href="my url">foo</a></b>'),
-        ("''italic''", "<i>italic</i>"),
-        ("'''''[[foo]]'''''", '<i><b><a href="my url">foo</a></b></i>'),
-        ("[[foo|bar]]", '<a href="my url">bar</a>'),
-        ("foo&nbsp;bar", "foo&nbsp;bar"),
-    ]
-)
-def testcase(request, Page, Article):
-    Article(None).url.return_value = "my url"
-    TestCase = namedtuple("TestCase", "input output")
-    return TestCase(*request.param)
+class TestRender:
+    @pytest.fixture(
+        params=[
+            ("", ""),
+            ("foo", "foo"),
+            ("[[foo]]", '<a href="my url">foo</a>'),
+            ("'''foo'''", "<b>foo</b>"),
+            ("'''[[foo]]'''", '<b><a href="my url">foo</a></b>'),
+            ("''italic''", "<i>italic</i>"),
+            ("'''''[[foo]]'''''", '<i><b><a href="my url">foo</a></b></i>'),
+            ("[[foo|bar]]", '<a href="my url">bar</a>'),
+            ("foo&nbsp;bar", "foo&nbsp;bar"),
+        ]
+    )
+    def testcase(self, request, Page, Article):
+        Article(None).url.return_value = "my url"
+        TestCase = namedtuple("TestCase", "input output")
+        return TestCase(*request.param)
 
+    def test_returns_correct_string(self, site, testcase):
+        hook = Hook("", testcase.input)
+        assert hook.render(site) == testcase.output
 
-def test_render(site, testcase):
-    hook = Hook("", testcase.input)
-    assert hook.render(site) == testcase.output
+    def test_logs_on_unknown_node(self, mocker, site, caplog):
+        parse = mocker.patch("dyk_tools.wiki.hook.parse", autospec=True)
+        parse("").nodes = [None]
+        hook = Hook(None, "")
+        hook.render(site)
+        assert "Unknown node type" in caplog.text
 
-
-def test_render_logs_on_unknown_node(mocker, site, caplog):
-    parse = mocker.patch("dyk_tools.wiki.hook.parse", autospec=True)
-    parse("").nodes = [None]
-    hook = Hook(None, "")
-    hook.render(site)
-    assert "Unknown node type" in caplog.text
-
-
-def test_render_does_not_log_with_no_unknown_node(mocker, site, caplog, Page):
-    hook = Hook("", r"that '''[[Edward B. Barry]]''' demerited 'humming'?")
-    hook.render(site)
-    assert "Unknown node type" not in caplog.text
+    def test_does_not_log_with_no_unknown_node(self, mocker, site, caplog, Page):
+        hook = Hook("", r"that '''[[Edward B. Barry]]''' demerited 'humming'?")
+        hook.render(site)
+        assert "Unknown node type" not in caplog.text
