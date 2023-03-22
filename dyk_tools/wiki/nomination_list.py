ea62218729b118c842d067df31dba7103c1f5680
Roy Smith <roy@panix.com>
2023-03-22 12:16:16 -0400

    Add NominationList.remove_nomination()

--- dyk_tools/wiki/nomination_list.py
+++ dyk_tools/wiki/nomination_list.py
@@ -16,3 +16,46 @@ class NominationList:
             recursive=False, matches="template:did you know nominations/"
         ):
             yield Nomination(Page(self.page, t.name))
+
+    def remove_nomination(self, old_page: Page, message: str) -> int:
+        """Remove a nomination transclusion and save the NominationList
+        back to the wiki, using ''message'' as the edit summary.
+
+        Returns the number of transclusions which were removed.  The current
+        implementation will remove all instances if old_page is transcluded more than
+        once.  It's unclear if this is the desired behavior, and may change in the
+        future.
+
+        Raises ValueError if old_page.title() doesn't look like a legitimate
+        DYK nomination template.
+
+        """
+        title = old_page.title()
+        if not title.lower().startswith("template:did you know nominations/"):
+            raise ValueError(f"'{title}' is not a valid DYK nomination page title")
+        wikicode = mwp.parse(self.page.get())
+        count = 0
+        # If (as is the most common case) a deleted transclusion is by itself on a line,
+        # we don't want to leave behind an empty line.  To handle this, we maintain a
+        # small state machine which recognizes a "newline, template, newline" sequence
+        # allowing us to delete the second newline.  We can't mutate the wikicode while
+        # we're iterating through it, so we just keep track of what nodes need to be
+        # deleted and delete them all in a second pass.
+        state = ""
+        nodes_to_remove = []
+        for node in wikicode.ifilter(recursive=False):
+            if isinstance(node, mwp.nodes.Text) and node.value == "\n":
+                if state == "template":
+                    nodes_to_remove.append(node)
+                state = "newline"
+            elif isinstance(node, mwp.nodes.Template) and node.name.matches(title):
+                state = "template" if state == "newline" else ""
+                nodes_to_remove.append(node)
+                count += 1
+            else:
+                state = ""
+        for node in nodes_to_remove:
+            wikicode.remove(node)
+        self.page.text = str(wikicode)
+        self.page.save(summary=message)
+        return count
--- tests/wiki/test_nomination_list.py
+++ tests/wiki/test_nomination_list.py
@@ -29,7 +29,6 @@ def test_nominations(mocker, page):
         ===Articles created/expanded on December 31===
         {{Template:Did you know nominations/Del Riley (clerk)}}
         {{Template:Did you know nominations/Braxton Cook}}
-        {{Template:Did you know nominations/Niwar (cotton tape)}}
         ===Articles created/expanded on January 15===
         {{Template:Did you know nominations/Piri (singer)}}
         """
@@ -37,13 +36,48 @@ def test_nominations(mocker, page):
     mocker.patch("dyk_tools.wiki.nomination_list.Page", new=MockPage)
     nomlist = NominationList(page)
 
-    nom_generator = nomlist.nominations()
+    noms = list(nomlist.nominations())
 
-    noms = list(nom_generator)
     titles = [nom.title() for nom in noms]
     assert titles == [
         "Template:Did you know nominations/Del Riley (clerk)",
         "Template:Did you know nominations/Braxton Cook",
-        "Template:Did you know nominations/Niwar (cotton tape)",
         "Template:Did you know nominations/Piri (singer)",
     ]
+
+
+def test_remove_nomination(mocker, site, page):
+    page.get.return_value = dedent(
+        """
+        ===Articles created/expanded on December 31===
+        {{Template:Did you know nominations/Nom 1}}
+        {{Template:Did you know nominations/Nom 2}}
+        ===Articles created/expanded on January 15===
+        {{Template:Did you know nominations/Nom 3}}
+        """
+    )
+    mocker.patch("dyk_tools.wiki.nomination_list.Page", new=MockPage)
+    nomlist = NominationList(page)
+    old_nom_page = MockPage(site, "Template:Did you know nominations/Nom 1")
+
+    count = nomlist.remove_nomination(old_nom_page, "test message")
+
+    nomlist.page.save.assert_called_once_with(summary="test message")
+    assert count == 1
+    expected_text = dedent(
+        """
+        ===Articles created/expanded on December 31===
+        {{Template:Did you know nominations/Nom 2}}
+        ===Articles created/expanded on January 15===
+        {{Template:Did you know nominations/Nom 3}}
+        """
+    )
+    assert nomlist.page.text == expected_text
+
+
+def test_remove_nomination_raises_on_invalid_template(mocker, site, page):
+    nomlist = NominationList(page)
+    old_nom_page = MockPage(site, "Template:Foo")
+
+    with pytest.raises(ValueError, match="'Template:Foo' .* page title"):
+        nomlist.remove_nomination(old_nom_page, "test message")
