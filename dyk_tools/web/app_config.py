db491420763ebc2dab8375d15a320b1613f4fa36
Roy Smith <roy@panix.com>
2024-11-24 13:36:36 -0500

    Look for dyk-tools.toml in DYK_TOOLS_BASEDIR instead of $HOME.

--- dyk_tools/web/app_config.py
+++ dyk_tools/web/app_config.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 
 import toml
 
-app_config = toml.load(Path.home() / "dyk-tools.toml")
+base_dir = os.environ["DYK_TOOLS_BASEDIR"]
+app_config = toml.load(Path(base_dir) / "dyk-tools.toml")
