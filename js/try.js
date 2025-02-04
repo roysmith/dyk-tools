bb1f0305ea848f5dbda1a8179b3d7f96e5850873
Roy Smith <roy@panix.com>
2025-02-03 22:07:24 -0500

    Initial working version

--- js/try.js
+++ js/try.js
@@ -1,3 +1,7 @@
+ "use strict";
+ 
+// import { cdxIconBellOutline } from '@wikimedia/codex-icons';
+
 mw.hook('wikipage.content').add(function ( $content ) {
     console.log('try.js loaded');
     // If the content is not a valid jQuery object, we can't do anything.
@@ -5,35 +9,27 @@ mw.hook('wikipage.content').add(function ( $content ) {
         console.log('not a valid jQuery object');
         return;
     }
-    const $users = $content.find( "a.userlink[href ^= '/wiki/User:']" );
-    console.log($content);
+    const pageName = mw.config.get( 'wgPageName' );
+    const dykNomPattern = new RegExp('^Template:Did_you_know_nominations/');
+    if (!dykNomPattern.test(pageName)) {
+      return;
+    }
+    const userSubpagePattern = new RegExp('^/wiki/User:[^/]+$');
+    const $users = $content.find( 'a' )
+        .filter(function (index) {
+            return userSubpagePattern.test($(this).attr('href'));
+        });
+    console.log( $content );
     console.log( $users );
 
-    // Add a button to each user.
     $users.each(function () {
-        console.log( this );
-        var $this = $( this ),
-            $button = $( '<button>' )
-                .addClass( 'oo-ui-buttonElement' )
-                .text( mw.message( 'ping' ).plain() )
+        const $this = $( this );
+        const $button = $( '<button>' )
+                .text( 'ping' )
                 .on( 'click', function () {
-                    var $pre = $this.find( 'pre' ),
-                        text = $pre.text();
-
-                    if ( !$pre.length ) {
-                        return;
-                    }
-
-                    try {
-                        var result = eval( text );
-                        if ( result !== undefined ) {
-                            window.alert( result );
-                        }
-                    } catch ( e ) {
-                        window.alert( e );
-                    }
+                    console.log( 'ping' );
+                    $this.text( '@' + $this.text() );
                 } );
-
-        $this.prepend( $button );
+        $button.insertAfter( $this );
     } );
 });
