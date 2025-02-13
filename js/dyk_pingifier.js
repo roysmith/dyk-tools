b86d1e296416592cb5306d410fa1085becbe09cb
Roy Smith <roy@panix.com>
2025-02-12 23:43:18 -0500

    Provide L2 header

--- js/dyk_pingifier.js
+++ js/dyk_pingifier.js
@@ -5,7 +5,7 @@
 // Source at https://github.com/roysmith/dyk-tools/
 
 
-mw.hook('wikipage.content').add(function ($content) {
+mw.hook('wikipage.content').add(async function ($content) {
     console.log('try.js loaded');
     // If the content is not a valid jQuery object, we can't do anything.
     if (!$content.jquery) {
@@ -18,12 +18,37 @@ mw.hook('wikipage.content').add(function ($content) {
         return;
     }
 
-    const $pingBox = $('<textarea id="ping-box" rows="6"></textarea>');
+    var updateTimes = {};
+    const $pingBox = $('<textarea id="ping-box" rows="8"></textarea>');
     $pingBox.insertBefore('#firstHeading');
     const templateName = $('#firstHeading > span.mw-page-title-main')
         .text()
         .replace('Did you know nominations/', '');
     $pingBox.append('===[[', pageName, '|', templateName, ']]===\n');
+
+    $.get('/wiki/Template:Did_you_know/Queue/LocalUpdateTimes', function (localUpdateTimes) {
+        const queuePattern = new RegExp('(?<tag>Queue \\d)');
+        const prepPattern = new RegExp('(?<tag>Prep \\d)');
+        $(localUpdateTimes).find('table.wikitable > tbody > tr').each(function ($row) {
+            const cells = $($(this).find('td'));
+            if (cells.length < 1) {
+                return;
+            }
+            const time = cells[3].innerHTML.replace('<br>', '&nbsp;').trim();
+            const tags = $(cells[0]).text();
+            var found = tags.match(queuePattern);
+            if (found) {
+                console.log('found queue', found.groups.tag);
+                updateTimes[found.groups.tag] = time;
+            }
+            found = tags.match(prepPattern);
+            if (found) {
+                console.log('found prep', found.groups.tag);
+                updateTimes[found.groups.tag] = time;
+            }
+        });
+        console.log(updateTimes);
+    });
 
     const $copyButton = $('<button id="copy-button">Copy</button>')
         .on('click', async function () {
@@ -38,11 +63,30 @@ mw.hook('wikipage.content').add(function ($content) {
         });
     $copyButton.insertAfter('#ping-box');
 
-    const $linksButton = $('<button id="links-button">Links</button>')
+    const $l2Button = $('<button id="l2-button">Add L2 Header</button>')
         .on('click', async function () {
-            $pingBox.append('links\n');
+            const params = {
+                action: 'query',
+                prop: 'linkshere',
+                titles: pageName,
+                format: 'json',
+            };
+            const api = new mw.Api();
+            api.get(params)
+                .done(function (data) {
+                    const id = mw.config.get('wgArticleId');
+                    data.query.pages[id].linkshere.forEach(function (pageData) {
+                        const title = pageData.title;
+                        const titlePattern = new RegExp('^Template:Did you know/Queue/(?<n>\\d+)$');
+                        const match = title.match(titlePattern);
+                        if (match) {
+                            const key = 'Queue ' + match.groups.n;
+                            $pingBox.prepend('==[[', title, '|', key, ']] (', updateTimes[key], ')==\n\n');
+                        };
+                    })
+                });
         });
-    $linksButton.insertAfter('#ping-box');
+    $l2Button.insertAfter('#ping-box');
 
     const userSubpagePattern = new RegExp('^/wiki/User:[^/]+$');
     const $users = $content.find('a')
