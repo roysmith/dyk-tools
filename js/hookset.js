c38903c46750c79452c5af5013bc922b16880584
Roy Smith <roy@panix.com>
2025-03-18 20:15:51 -0400

    Initial version of HookSet

---
+++ js/hookset.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// hookset.js
+// Distributed under the MIT license
+// Source at https://github.com/roysmith/dyk-tools/
+
+class HookSet {
+    constructor(pageTitle) {
+        this.wikitext = "";
+        this.hooks = [];
+    }
+
+    async init(pageTitle) {
+        await this.load(pageTitle);
+        this.hooks = this.findHooks(this.wikitext);
+    }
+
+    async load(pageTitle) {
+        const api = new mw.Api();
+        const params = {
+            action: 'parse',
+            format: 'json',
+            page: pageTitle,
+            prop: 'wikitext',
+            formatversion: 2,
+        };
+        const self = this;
+        api.get(params)
+            .then((result) => {
+                const wikitext = JSON.parse(result).parse.wikitext;
+                self.wikitext = wikitext;
+            }, (result) => {
+                console.log(result);
+            });
+    }
+
+    findHooks(wikitext) {
+        const m = wikitext.match(new RegExp('^<!--Hooks-->$(?<block>.*)^<!--HooksEnd-->$', 'sm'));
+        return m.groups.block.split('\n')
+            .filter((line) => {
+                return line.startsWith('* ... ');
+            });
+    }
+}
+
+module.exports = { HookSet };
---
+++ js/hookset.test.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fs = require('node:fs');
+const { HookSet } = require('./hookset');
+const { describe } = require('node:test');
+
+function getDocument(pathName) {
+    return fs.readFileSync(pathName, 'utf8');
+}
+
+describe('constructor', () => {
+    it('builds a default instance', () => {
+        const hs = new HookSet();
+        expect(hs).toBeInstanceOf(HookSet);
+    });
+});
+
+describe('init', () => {
+    it('loads wikitext', async () => {
+        const json = getDocument('src/js/Template:Did_you_know/Queue/1@1280703597.json');
+        mw.Api.prototype.get = jest.fn()
+            .mockResolvedValue(json);
+
+        const hs = new HookSet();
+        await hs.init("My Queue");
+
+        expect(mw.Api.prototype.get).toHaveBeenCalledTimes(1);
+        expect(mw.Api.prototype.get).toHaveBeenCalledWith({
+            action: 'parse',
+            format: 'json',
+            page: "My Queue",
+            prop: 'wikitext',
+            formatversion: 2,
+        });
+        expect(hs.wikitext).toMatch(/^{{DYKbotdo.*}}/);
+    });
+
+    it('parses the hooks', async () => {
+        const json = JSON.stringify({
+            "parse": {
+                "wikitext": "Blah blah\n"
+                    + "<!--Hooks-->\n"
+                    + "{{main page image}}\n"
+                    + "* ... that politician '''[[Prasenjit Barman]]''' was credited for leading the restoration of the [[Cooch Behar Palace]]?\n"
+                    + "* ... that '''[[Sound Transit]]''' has 170 pieces of permanent public art at its stations and facilities?\n"
+                    + "<!--HooksEnd-->\n"
+                    + "{{flatlist|class=dyk-footer noprint|style=margin-top: 0.5em; text-align: right;}}\n"
+            }
+        });
+        mw.Api.prototype.get = jest.fn()
+            .mockResolvedValue(json);
+
+        const hs = new HookSet();
+        await hs.init("My Queue");
+
+        expect(hs.hooks).toEqual([
+            "* ... that politician '''[[Prasenjit Barman]]''' was credited for leading the restoration of the [[Cooch Behar Palace]]?",
+            "* ... that '''[[Sound Transit]]''' has 170 pieces of permanent public art at its stations and facilities?"
+        ]);
+    });
+});
+
