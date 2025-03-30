1c63bdfffa58cebd5b6bcc97ea900de042346aeb
Roy Smith <roy@panix.com>
2025-03-30 00:00:42 -0400

    Delete redundant test.

--- js/localUpdateTimes.test.js
+++ js/localUpdateTimes.test.js
@@ -46,25 +46,3 @@ describe('build', () => {
     });
 })
 
-describe('parseLocalUpdateTimes', () => {
-    it('finds the preps and queues', () => {
-        const html = getDocument('src/js/localUpdateTimes.html');
-        const updateTimes = LocalUpdateTimes.parse(html);
-        expect(updateTimes).toEqual({
-            'Queue 1': '17&nbsp;February&nbsp;00:00',
-            'Queue 2': '14&nbsp;February&nbsp;00:00',
-            'Queue 3': '14&nbsp;February&nbsp;12:00',
-            'Queue 4': '15&nbsp;February&nbsp;00:00',
-            'Queue 5': '15&nbsp;February&nbsp;12:00',
-            'Queue 6': '16&nbsp;February&nbsp;00:00',
-            'Queue 7': '16&nbsp;February&nbsp;12:00',
-            'Prep 1': '17&nbsp;February&nbsp;00:00',
-            'Prep 2': '17&nbsp;February&nbsp;12:00',
-            'Prep 3': '18&nbsp;February&nbsp;00:00',
-            'Prep 4': '18&nbsp;February&nbsp;12:00',
-            'Prep 5': '19&nbsp;February&nbsp;00:00',
-            'Prep 6': '16&nbsp;February&nbsp;00:00',
-            'Prep 7': '16&nbsp;February&nbsp;12:00',
-        });
-    });
-});
