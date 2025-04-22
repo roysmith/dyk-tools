80842cc6e53bb7a068a9d557c2bd7924dbc12a23
Roy Smith <roy@panix.com>
2025-04-22 13:26:42 -0400

    addReviewBox()

--- js/dyk_reviewTool.js
+++ js/dyk_reviewTool.js
@@ -1,15 +1,42 @@
+"use strict";
+
+// User:RoySmith/dyk-pingifier.js
+// Distributed under the MIT license
+// Source at https://github.com/roysmith/dyk-tools/
+
+const { LocalUpdateTimes } = require('./localUpdateTimes');
+const { HookSet } = require('./hookset');
+
 class DYKReviewTool {
-    constructor() {
+    constructor(lut, hookSet) {
+        this.localUpdateTimes = lut;
+        this.hookSet = hookSet;
     }
 
-    async install() {
-        mw.util.addPortletLink(
+    static async install() {
+        const link = mw.util.addPortletLink(
             'p-cactions',
             '#',
             'Review',
             'ca-review',
             'Review hookset',
         );
+        $(link).on('click', DYKReviewTool.review);
+    }
+
+    static async review() {
+        const lut = await LocalUpdateTimes.build();
+        const pageTitle = mw.config.get('wgPageName').replace(/_/g, ' ');
+        const hookSet = await HookSet.build(pageTitle);
+        const rt = new DYKReviewTool(lut, hookSet);
+        rt.addReviewBox();
+    }
+
+    addReviewBox() {
+        const key = this.hookSet.key();
+        const $reviewBox = $('<textarea id="dyk-review-box" rows="8"></textarea>')
+            .append(`==[[${this.hookSet.title}|${key}]] (${this.localUpdateTimes.updateTimes[key]})==\n`)
+            .insertBefore('#firstHeading');
     }
 }
 
@@ -19,6 +46,7 @@ if (typeof (module) != 'undefined') {
 
 mw.hook('wikipage.content').add(async function ($content) {
     if (mw.config.get('wgPageName').match(/^Template:Did_you_know\/Queue\//)) {
-        new DYKReviewTool().install();
+        DYKReviewTool.install();
+        DYKReviewTool.review(); // for testing
     }
 });
